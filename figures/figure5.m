9f32aa1195a790befae08fdb0fbc5375f303fd8c
Nate Linden <natejlinden@gmail.com>
2021-06-24 17:47:35 -0700

    make panes for figure 5

--- figures/figure5.m
+++ figures/figure5.m
@@ -1,20 +1,19 @@
 % ORIGINAL CODE FROM Linden et al. 2020 (Go with the FLOW: Visualizing spatiotemporal dynamics in1optical widefield calcium imaging)
 
 % This script recreates all the panes in Figure 5
-
+ 
 % NOTE: The colormaps here use system colormaps and differ from those in than the manuscript. See colormaps section below for details.
 
 clear all; close all; clc
 
-%%%% ADD REQUIRED CODE %%%%
+%%%% ADD REQUIRED CODE %%%% 
 addpath('../flow_portraits/')
 mkdir('./figure5/')
 
 
 %%%% COLORMAPS %%%%
 %   This script uses MATLAB colormaps by defualt, however these were not used in the manuscript.
-cmapl1 = colormap('gray');
-cmapl5 = colormap('gray');
+cmap = colormap('gray');
 
 % To exactly recreate the images in the manuscript please download and add ColorBrewer and Colorcet to the path:
 %   https://www.mathworks.com/matlabcentral/fileexchange/45208-colorbrewer-attractive-and-distinctive-colormaps
@@ -22,57 +21,60 @@ cmapl5 = colormap('gray');
 %   
 %   AND uncomment below
 %
-% cmapl1 = colorcet('L1');
-% cmapl5 = colorcet('L5');
+% cmap = colorcet('L5');
+
+%%%% load data %%%%
+% Pup Wave
+load('../data/fig5_pupPlaneWave.mat')
+% Traveling wave:
+load('../data/PlaneWave.mat');
 
 
-%%%% Two files corresponding the two waves in figure 5 %%%%
-files = {'../data/developing_wave1.mat', '../data/developing_wave2.mat'};
+%---- Save panes of data ----%
+planeFilename = './figure5/planeWave_';
+pupFilename = './figure5/pupWave_';
 
-%%%% PROCESS AND PLOT EACH WAVE %%%%
-for wave = 1:2
-    % load data
-    load(files{wave});
-    dfof = dat.dfof; mask = dat.mask;
-    clear dat;
+planeStride = 5; planeIdxs = 15:planeStride:40;
+pupStride = 5; pupIdxs = 10:pupStride:size(pupWave,3); 
+num_panes = 6;
 
-    % Create flow portrait to overlay
-    integrationLength = 40;
-    filename = strcat('./figure5/wave_', num2str(wave), '_flowPortrait.png');
-    flowPortrait(dfof, integrationLength, 'filename', filename, 'mask', mask, 'background_cmap', cmapl1, 'white_background', true, 'thresh_quantile', 0.92);
 
-    %% Save panes 
-    fig_size = [0 0 180 173];
+for i = 1:num_panes
 
-    temp = dfof(:,:,70:end-39);
-    num_frames = 12;
-    idxs = linspace(1, size(temp,3),num_frames);
-
-    for j = 1:num_frames
-        frm = idxs(j);
-        figure('Renderer', 'painters', 'Position', fig_size);
-        frame = temp(:,:,round(frm));
-
-        [newImg, imAlpha] = whiteBackground(frame, mask);
-        imagesc(newImg, 'AlphaData',imAlpha); 
-        
-        % plot formatting
-        colormap(cmapl5)
-        % gc = gcf; gc.Position = fig_size;
-        axis off;
-        
-        ax = gca; 
-        ax.Position = ax.OuterPosition;
-        % ax1 = hi.Parent;
-        % ax1.Position = ax.OuterPosition;
-        filename = strcat('./figure5/wave_',num2str(wave),'_frame_',num2str(round(frm)),'.png');
-        saveas(gcf, filename);
-    
-        disp(frm)
-        close gcf
-
+    %%%% plane wave
+    figure('Renderer', 'painters', 'Position', [0 0 1000 1000],'color', 'w');
+    imagesc(planeWave(:,:,planeIdxs(i))); 
+    axis off; ax = gca;
+    ax.Position = ax.OuterPosition;
+    if ~sum(sum(planeWave(:,:,planeIdxs(i))))
+        ax.CLim = [0 1];
     end
+    colormap(cmap)
+    saveas(gcf, strcat(planeFilename, num2str(planeIdxs(i)), '.png'));
+    close all
 
+    %%%% pup wave
+    figure('Renderer', 'painters', 'Position', [0 0 1000 1000],'color', 'w');
+    imagesc(pupWave(:,:,pupIdxs(i)));
+    axis off; ax = gca;
+    scale = [min(min(min(pupWave))), max(max(max(pupWave)))];
+    ax.Position = ax.OuterPosition;
+    ax.CLim = scale;
+    colormap(cmap)
+    saveas(gcf, strcat(pupFilename, num2str(pupIdxs(i)), '.png'));
     close all
 
 end
+
+%---- FLOW Portraits ----%
+%%%% traveling wave
+filename = './figure5/planeWave_';
+int_len = 15;
+flowPortraitR2020b(planeWave(:,:,10:end), int_len, 'history_delay',0, 'filename',filename,'img_format','.png', 'save_flow',false, 'thresh_quantile', 0.91);
+
+%%%% pup wave
+filename = './figure5/pupWave_';
+int_len = 5;
+hist_delay = 10;
+flowPortraitR2020b(pupWave, int_len, 'history_delay',hist_delay,'filename',filename,'img_format','.png', 'save_flow',false, 'thresh_quantile', 0.90,...
+'figsize', [0 0 500 500]);
